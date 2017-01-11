f36b56ea23aba2d90849a660dc5b1fb220c405fb
Josh Ho <hojo1025@gmail.com>
2017-01-10 22:27:27 -0500

    added user question stories

---
+++ app/controllers/questions_controller.rb
@@ -0,0 +1,29 @@
+class QuestionsController < ApplicationController
+  def index
+    @questions = Question.order(created_at: :desc)
+  end
+
+  def show
+    @question = Question.find(params[:id])
+  end
+
+  def new
+    @question = Question.new
+  end
+
+  def create
+    @question = Question.new(question_params)
+    if @question.save
+      redirect_to @question, notice: 'Question was successfully created.'
+    else
+      render :new
+    end
+  end
+
+
+
+  def question_params
+    params.require(:question).permit(:title, :description)
+  end
+
+end
---
+++ app/models/question.rb
@@ -0,0 +1,5 @@
+class Question < ApplicationRecord
+
+  validates :title, presence: true, length: {minimum: 40}
+  validates :description, presence: true, length: {minimum: 40}
+end
